2a79cc9f2fe7a361a1d3cd25f647ac73a7e2a859
tangjian <406158101@qq.com>
2024-03-10 12:01:50 +0800

    更新Test.cs和Assembly-CSharp.csproj文件

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,19 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    [serializeField]
-    private Gameobject instancing;
+    [SerializeField]
+    private GameObject instancing;
 
     [SerializeField]
-    private Gameobject normal;
+    private GameObject normal;
+
+    [SerializeField]
+    private bool isInstancing;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        // 生成1000个到随机位置
+        for (int i = 0; i < 1000; i++)
+        {
+            if (isInstancing)
+            {
+                GameObject go = Instantiate(instancing);
+                go.name = instancing.name;
+                go.transform.position = new Vector3(Random.Range(-10f, 10f), 0, Random.Range(-10f, 10f));
+            }
+            else
+            {
+                GameObject go = Instantiate(normal);
+                go.name = normal.name;
+                go.transform.position = new Vector3(Random.Range(-10f, 10f), 0, Random.Range(-10f, 10f));
+            }
+        }
     }
 
     // Update is called once per frame
