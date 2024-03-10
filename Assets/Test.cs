a188c982e51e0e91c36272f80d47486c3e556ec6
tangjian <406158101@qq.com>
2024-03-10 11:16:10 +0800

    添加新文件和修改代码

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    [serializeField]
+    private Gameobject instancing;
+
+    [SerializeField]
+    private Gameobject normal;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
